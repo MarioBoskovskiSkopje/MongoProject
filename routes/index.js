b9afe47ff186b825c36889b7bcc1fcfca4c6f9fa
unknown <oliveraluloski@gmail.com>
2018-09-19 17:39:05 +0200

    Updated Routes

--- routes/index.js
+++ routes/index.js
@@ -24,21 +24,33 @@ const BrandsRoutes = require("./BrandsRoutes.js");
 const ColorRoutes = require("./ColorRoutes.js");
 const CustomersRoutes = require("./CustomersRoutes.js");
 const FillLevelsRoutes = require("./FillLevelsRoutes.js");
+const FlavourMixesRoutes = require("./FlavourMixesRoutes");
+const FlavourMixOrdersRoutes = require("./FlavourMixOrdersRoutes");
 const FlavoursRoutes = require("./FlavoursRoutes.js");
+const highestBatchRoutes = require("./highestBatchRoutes.js");
 const InventoryRoutes = require("./InventoryRoutes.js");
+const LabellingRoutes = require("./LabellingRoutes.js");
 const LabelsRoutes = require("./LabelsRoutes.js");
 const LidsRoutes = require("./LidsRoutes.js");
 const LogRoutes = require("./LogRoutes.js");
 const MachinesRoutes = require("./MachinesRoutes.js");
 const MiscRoutes = require("./MiscRoutes.js");
 const MixingRoutes = require("./MixingRoutes.js");
+const MixingTeamRoutes = require("./MixingTeamRoutes.js");
+const OrdersRoutes = require("./OrdersRoutes.js");
 const PackagesRoutes = require("./PackagesRoutes.js");
+const PackagingRoutes = require("./PackagingRoutes.js");
+const PremixColoringRoutes = require("./PremixColoringRoutes.js");
 const PremixesTypesRoutes = require("./PremixesTypesRoutes.js");
+const PrintingRoutes = require("./PrintingRoutes.js");
+const ProductionRoutes = require("./ProductionRoutes.js");
 const RecipesRoutes = require("./RecipesRoutes.js");
+const ReferencesRoutes = require("./ReferencesRoutes.js");
 const SchedulesBreaksRoutes = require("./Schedules BreaksRoutes.js");
-const UnbrandedTypesRoutes = require("./UnbrandedTypesRoutes.js");
 const SchedulesReferenceRoutes = require("./Schedules ReferenceRoutes");
-const FlavourMixes = require("./FlavourMixesRoutes");
+const SchedulesRoutes = require("./SchedulesRoutes.js");
+const ShippingRoutes = require("./ShippingRoutes.js");
+const UnbrandedTypesRoutes = require("./UnbrandedTypesRoutes.js");
 
 module.exports = {
   BottleTypesRoutes,
@@ -49,18 +61,30 @@ module.exports = {
   CustomersRoutes,
   FillLevelsRoutes,
   FlavoursRoutes,
+  FlavourMixesRoutes,
+  FlavourMixOrdersRoutes,
+  highestBatchRoutes,
   InventoryRoutes,
   LabelsRoutes,
+  LabellingRoutes,
   LidsRoutes,
   LogRoutes,
   MachinesRoutes,
   MiscRoutes,
   MixingRoutes,
+  MixingTeamRoutes,
+  OrdersRoutes,
   PackagesRoutes,
+  PackagingRoutes,
   PremixesTypesRoutes,
+  PremixColoringRoutes,
+  PrintingRoutes,
+  ProductionRoutes,
   RecipesRoutes,
+  ReferencesRoutes,
   SchedulesBreaksRoutes,
-  UnbrandedTypesRoutes,
   SchedulesReferenceRoutes,
-  FlavourMixes
+  SchedulesRoutes,
+  ShippingRoutes,
+  UnbrandedTypesRoutes
 };
